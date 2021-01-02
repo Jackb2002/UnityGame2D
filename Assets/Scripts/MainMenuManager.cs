7182135de485666609795570622828f2c9ce366d
Jack B <jackbassilious@hotmail.co.uk>
2021-01-02 21:10:03 +0000

    Started adding base multiplyer code and created dufferbece between playing and testing.  Added basic main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Multiplayer()
+    {
+
+    }
+
+    public void LevelDesigner()
+    {
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+}
